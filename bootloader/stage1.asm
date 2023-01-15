881f1c9608acd25b6b8600e48414648d479b407f
botondmester <boboooka1@gmail.com>
2023-01-15 11:28:42 +0100

    clear screen

--- bootloader/stage1.asm
+++ bootloader/stage1.asm
@@ -2,12 +2,21 @@
 [org 0x9000]
 
 begin:
+    call cls
     mov bx, .msg
     call print
     call print_nl
     jmp $
 
     .msg: db "Stage1 loaded!", 0
+
+cls:
+    pusha
+    mov ah, 0x00
+    mov al, 0x03 ; text mode 80x25 16 colors
+    int 0x10
+    popa
+    ret
 
 %include "16bit_print.asm"
 
