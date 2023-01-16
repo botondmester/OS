3d788781962e71ba528efb7d4ccb28fbb69b0b13
botondmester <boboooka1@gmail.com>
2023-01-16 17:43:58 +0100

    fix typo :|

--- kernel/main.c
+++ kernel/main.c
@@ -5,5 +5,5 @@ kmain(){
     GDT_Initialize();
     print_clear();
     print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
-    print_str("Welcome to our 64-bit kernel!");
+    print_str("Welcome to our 32-bit kernel!");
 }
