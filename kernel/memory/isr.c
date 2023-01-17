faec96529d3a25cb7908a843230a2493dff699da
botondmester <boboooka1@gmail.com>
2023-01-17 19:53:15 +0100

    Paging

--- kernel/memory/isr.c
+++ kernel/memory/isr.c
@@ -11,33 +11,34 @@ void register_interrupt_handler(uint8_t n, isr_t handler)
 
 void isr_handler(registers_t regs)
 {
-    uint32_t tmp = regs.int_no;
-    uint8_t int_no = tmp;
-    tmp = regs.err_code;
-    
-    print_str("recieved interrupt: ");
-    print_hex8(int_no);
-    print_char('\n');
-    if(tmp!=0){
-        print_str("error code: ");
-        print_hex32(tmp);
+    if (interrupt_handlers[regs.int_no] != 0)
+    {
+        isr_t handler = interrupt_handlers[regs.int_no];
+        handler(regs);
+    }
+    else {
+        print_str("recieved interrupt: ");
+        print_hex8(regs.int_no);
         print_char('\n');
+        if(regs.err_code!=0){
+            print_str("error code: ");
+            print_hex32(regs.err_code);
+            print_char('\n');
+        }
     }
 }
 
 void irq_handler(registers_t regs)
 {
-    uint32_t tmp = regs.int_no;
-    uint8_t int_no = tmp;
-    if (int_no >= 40)
+    if (regs.int_no >= 40)
     {
         outb(0xA0, 0x20);
     }
     outb(0x20, 0x20);
 
-    if (interrupt_handlers[int_no] != 0)
+    if (interrupt_handlers[regs.int_no] != 0)
     {
-        isr_t handler = interrupt_handlers[int_no];
+        isr_t handler = interrupt_handlers[regs.int_no];
         handler(regs);
     }
 }
